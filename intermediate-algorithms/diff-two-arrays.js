5da3fa69a9a3d1e51dc38b313246ed4c9654bf12
Amy Carney <digilou@users.noreply.github.com>
2017-01-17 06:55:35 -0900

    Started file

---
+++ intermediate-algorithms/diff-two-arrays.js
@@ -0,0 +1,14 @@
+/******************
+Compare two arrays and return a new array with any items only found in one of the two given arrays, but not both. In other words, return the symmetric difference of the two arrays.
+Comparison Operators
+Array.prototype.slice()
+Array.prototype.filter()
+Array.prototype.indexOf()
+Array.prototype.concat()
+*******************/
+
+function diffArray(arr1, arr2) {
+  var newArr = [];
+  // Same, same; but different.
+  return newArr;
+}
