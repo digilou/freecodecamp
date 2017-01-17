8060e5374babaafe00ad5b15a3dae4fd7fe8ecb8
Amy Carney <digilou@users.noreply.github.com>
2017-01-17 06:48:14 -0900

    Updated solution

--- intermediate-algorithms/sum-all-numbers.js
+++ intermediate-algorithms/sum-all-numbers.js
@@ -8,9 +8,16 @@ Array.prototype.reduce()
 // answer started.....
 
 function sumAll(arr) {
-  return arr.reduce(function(a,b){
-    return a + b;
-  }, 0);
+  // find lowest number
+  var min = Math.min(arr[0], arr[1]);
+  // find highest number
+  var max = Math.max(arr[0], arr[1]);
+  // create placeholder for numbers to add up
+  var num = 0;
+  // iterate through lowest to highest number to get the sum
+  for(var i = min; i <= max; i++){
+    num += i;
+  }
+  // don't forget to return the final sum
+  return num;
 }
-
-sumAll([1, 4]);
