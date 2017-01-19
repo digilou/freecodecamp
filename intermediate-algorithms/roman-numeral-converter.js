5a9152cf01fe4255f07f4f82327c45891ecb0bfb
Amy Carney <digilou@users.noreply.github.com>
2017-01-19 06:57:01 -0900

    Added new algorithm

---
+++ intermediate-algorithms/roman-numeral-converter.js
@@ -0,0 +1,16 @@
+/*************
+Convert the given number into a roman numeral.
+
+All roman numerals answers should be provided in upper-case.
+
+Roman Numerals
+Array.prototype.splice()
+Array.prototype.indexOf()
+Array.prototype.join()
+***************/
+
+function convertToRoman(num) {
+ return num;
+}
+
+convertToRoman(36);
