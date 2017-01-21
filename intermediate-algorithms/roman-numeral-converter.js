1e326cb442875fc06560d955476814fa2a7a74c4
Amy Carney <digilou@users.noreply.github.com>
2017-01-21 11:53:28 -0900

    Added variables

--- intermediate-algorithms/roman-numeral-converter.js
+++ intermediate-algorithms/roman-numeral-converter.js
@@ -10,6 +10,10 @@ Array.prototype.join()
 ***************/
 
 function convertToRoman(num) {
+  // create arrays for basic Roman Numerals and decimals
+  var romanNum = ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"];
+  var decimalNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20, 30, 40, 50, 60, 70, 80, 90];
+  
   // break apart the digits
   num.splice('');
   
