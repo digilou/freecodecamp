c90f96045fa4034202d170316e775cfcd2950a8d
Amy Carney <digilou@users.noreply.github.com>
2017-02-05 13:36:06 -0900

    Added final solution

--- intermediate-algorithms/search-and-replace.js
+++ intermediate-algorithms/search-and-replace.js
@@ -16,10 +16,15 @@ Array.prototype.join()
 
 ******************/
 function myReplace(str, before, after) {
-  // replace second argument with third argument
+  // Find index where before is on string
+  var index = str.indexOf(before);
+  // Check to see if the first letter is uppercase or not
+  if (str[index] === str[index].toUpperCase()) {
+    // Change the after word to be capitalized before we use it.
+    after = after.charAt(0).toUpperCase() + after.slice(1);
+  }
+  // Now replace the original str with the edited one.
   str = str.replace(before, after);
-  // need to preserve case somehow
-  
   return str;
 }
 
