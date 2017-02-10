c74006f2f6f393b2cd57431d14b473ee813ecf88
Amy Carney <digilou@users.noreply.github.com>
2017-02-09 21:36:55 -0900

    Init missing-letters

---
+++ intermediate-algorithms/missing-letters.js
@@ -0,0 +1,9 @@
+/*********
+Find the missing letter in the passed letter range and return it.
+
+If all letters are present in the range, return undefined.
+
+    String.prototype.charCodeAt()
+
+    String.fromCharCode()
+*********/
