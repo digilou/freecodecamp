2fc0a2cf9f6ee30bc4aa9f3740e0da48bda1bae7
Amy Carney <digilou@users.noreply.github.com>
2017-02-11 16:37:23 -0900

    Jotted ideas for solution

--- intermediate-algorithms/convert-html-entities.js
+++ intermediate-algorithms/convert-html-entities.js
@@ -7,3 +7,15 @@ Convert the characters &, <, >, " (double quote), and ' (apostrophe), in a strin
 
 ********/
 
+function convertHTML(str) {
+  var regExp = new RegExp(['&', '<', '>', '"', "'"], "g","i");
+  switch(str){
+    case '&':
+      str = "&amp;";
+      break;
+  }
+  // &amp; &lt; &gt; &dquo; &apo;
+  return str;
+}
+
+convertHTML("Dolce & Gabbana");
