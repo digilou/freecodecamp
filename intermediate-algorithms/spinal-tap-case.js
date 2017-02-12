5e284700f43533b4f8e016d8121dfac4fb1a91ef
Amy Carney <digilou@users.noreply.github.com>
2017-02-12 07:45:31 -0900

    Init spinal-tap-case

---
+++ intermediate-algorithms/spinal-tap-case.js
@@ -0,0 +1,14 @@
+/********
+Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.
+
+RegExp
+String.prototype.replace()
+********/
+
+function spinalCase(str) {
+  // "It's such a fine line between stupid, and clever."
+  // --David St. Hubbins
+  return str;
+}
+
+spinalCase('This Is Spinal Tap');
