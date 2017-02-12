f0a05ef5ca3a31ebdd99408beba0432481a9c433
Amy Carney <digilou@users.noreply.github.com>
2017-02-11 16:13:45 -0900

    Init Convert HTML Entities challenge

---
+++ intermediate-algorithms/convert-html-entities.js
@@ -0,0 +1,9 @@
+/********
+Convert the characters &, <, >, " (double quote), and ' (apostrophe), in a string to their corresponding HTML entities.
+
+- RegExp
+- HTML Entities
+- String.prototype.replace()
+
+********/
+
