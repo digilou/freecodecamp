8ba4c581b9b30add2b8c2d562c7092518342806f
Amy Carney <digilou@users.noreply.github.com>
2017-02-16 16:18:42 -0900

    Starting Over...

--- intermediate-algorithms/smallest-common-multiple.js
+++ intermediate-algorithms/smallest-common-multiple.js
@@ -9,29 +9,6 @@ e.g. for 1 and 3 - find the smallest common multiple of both 1 and 3 that is eve
 ********/
 
 function smallestCommons(arr) {
-  // Create new array. Add all values from greater to smaller from the
-  // original array.
-  var newArr = [];
-  for (var i = arr[0]; i >= arr[1]; i--) {
-    newArr.push(i);
-  }
-
-  var lcm = 0;
-  var loop = 1;
-  var num;
-
-  // While n is not the same as the array length.
-  do {
-    lcm = newArr[0] * loop * newArr[1];
-    for (num = 2; num < newArr.length; num++) {
-      if (lcm % newArr[num] !== 0) {
-        break;
-      }
-    }
-
-    loop++;
-  } while (num !== newArr.length);
-
   return lcm;
 }
 
