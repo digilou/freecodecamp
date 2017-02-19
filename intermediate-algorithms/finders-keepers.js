7323a59502dfc7665533debdff702d61d583b031
Amy Carney <digilou@users.noreply.github.com>
2017-02-18 16:26:37 -0900

    Init finders-keepers.js

---
+++ intermediate-algorithms/finders-keepers.js
@@ -0,0 +1,14 @@
+/********
+Create a function that looks through an array (first argument) and returns the first element in the array that passes a truth test (second argument).
+
+Array.prototype.filter()
+********/
+
+function findElement(arr, func) {
+  // Create a function that looks through an array (first argument) 
+  // and returns the first element in the array that passes a truth test (second argument).
+  var num = 0;
+  return num;
+}
+
+findElement([1, 2, 3, 4], function(num){ return num % 2 === 0; });
