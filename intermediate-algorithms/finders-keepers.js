efda3603a19486be98abeb99b94b13a818a1a816
Amy Carney <digilou@users.noreply.github.com>
2017-02-19 14:57:03 -0900

    Final solution(s) to finders-keepers

--- intermediate-algorithms/finders-keepers.js
+++ intermediate-algorithms/finders-keepers.js
@@ -4,11 +4,23 @@ Create a function that looks through an array (first argument) and returns the f
 Array.prototype.filter()
 ********/
 
+// Soluiton using for loop
 function findElement(arr, func) {
-  // Create a function that looks through an array (first argument) 
-  // and returns the first element in the array that passes a truth test (second argument).
-  var num = 0;
+  let num = 0;
+  for(let i = 0; i <= arr.length; i++){
+    if(func(arr[i])){
+       num = arr[i];
+       return num;
+     }
+   }
   return num;
 }
 
+// Solution using .filter()function findElement(arr, func) {
+function findElement (arr, func){
+  var filtered = arr.filter(func);
+  return filtered[0];
+}
+
+// test
 findElement([1, 2, 3, 4], function(num){ return num % 2 === 0; });
