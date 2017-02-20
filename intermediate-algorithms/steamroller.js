e4f0bc4f06d604a4805a39b037638ad66e6bb232
Amy Carney <digilou@users.noreply.github.com>
2017-02-19 15:26:06 -0900

    Init steamroller.js

---
+++ intermediate-algorithms/steamroller.js
@@ -0,0 +1,12 @@
+/********
+Flatten a nested array. You must account for varying levels of nesting.
+
+- Array.isArray()
+********/
+
+function steamrollArray(arr) {
+  // I'm a steamroller, baby
+  return arr;
+}
+
+steamrollArray([1, [2], [3, [[4]]]]);
