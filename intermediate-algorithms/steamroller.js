dfd1d781281eb333182140bffd5656a5062e3356
Amy Carney <digilou@users.noreply.github.com>
2017-02-20 14:20:44 -0900

    Final solution

--- intermediate-algorithms/steamroller.js
+++ intermediate-algorithms/steamroller.js
@@ -5,8 +5,11 @@ Flatten a nested array. You must account for varying levels of nesting.
 ********/
 
 function steamrollArray(arr) {
-  // I'm a steamroller, baby
-  return arr;
+  // flatten array with recursion and checking if each element isArray to flatten and concat
+  return arr.reduce(function (flat, toFlatten) {
+    return flat.concat(Array.isArray(toFlatten) ? steamrollArray(toFlatten) : toFlatten);
+  }, []);
 }
+
 
 steamrollArray([1, [2], [3, [[4]]]]);
