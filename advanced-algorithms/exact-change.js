9a9c808bd594c6fcee8ad468bd4d25acf7a26eb2
Amy Carney <digilou@users.noreply.github.com>
2017-02-26 17:50:24 -0900

    Started solution...

--- advanced-algorithms/exact-change.js
+++ advanced-algorithms/exact-change.js
@@ -11,22 +11,25 @@ Otherwise, return change in coin and bills, sorted in highest to lowest order.
 - Global Object
 ********/
 
+// create object array for coins and bills
+const moneys = [
+  {"ONE HUNDRED", 100.00},
+  {"TWENTY", 20.00},
+  {"TEN", 10.00},
+  {"FIVE", 5.00},
+  {"ONE", 1.00},
+  {"QUARTER", 0.25},
+  {"DIME", 0.10},
+  {"NICKEL", 0.05},
+  {"PENNY", 0.01}
+  ]
 
 function checkCashRegister(price, cash, cid) {
-  var change;
+  var change = cash - price;
+  
   // Here is your change, ma'am.
   return change;
 }
 
-// Example cash-in-drawer array:
-// [["PENNY", 1.01],
-// ["NICKEL", 2.05],
-// ["DIME", 3.10],
-// ["QUARTER", 4.25],
-// ["ONE", 90.00],
-// ["FIVE", 55.00],
-// ["TEN", 20.00],
-// ["TWENTY", 60.00],
-// ["ONE HUNDRED", 100.00]]
 
 checkCashRegister(19.50, 20.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]]);
