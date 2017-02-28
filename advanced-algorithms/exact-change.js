d3769f06b51747480791617b152df8ca99107dec
Amy Carney <digilou@users.noreply.github.com>
2017-02-28 09:01:06 -0900

    Final solution

--- advanced-algorithms/exact-change.js
+++ advanced-algorithms/exact-change.js
@@ -13,23 +13,59 @@ Otherwise, return change in coin and bills, sorted in highest to lowest order.
 
 // create object array for coins and bills
 const moneys = [
-  {"ONE HUNDRED", 100.00},
-  {"TWENTY", 20.00},
-  {"TEN", 10.00},
-  {"FIVE", 5.00},
-  {"ONE", 1.00},
-  {"QUARTER", 0.25},
-  {"DIME", 0.10},
-  {"NICKEL", 0.05},
-  {"PENNY", 0.01}
-  ]
+  { name: 'ONE HUNDRED', val: 100.00},
+  { name: 'TWENTY', val: 20.00},
+  { name: 'TEN', val: 10.00},
+  { name: 'FIVE', val: 5.00},
+  { name: 'ONE', val: 1.00},
+  { name: 'QUARTER', val: 0.25},
+  { name: 'DIME', val: 0.10},
+  { name: 'NICKEL', val: 0.05},
+  { name: 'PENNY', val: 0.01}
+];
 
 function checkCashRegister(price, cash, cid) {
   var change = cash - price;
-  
-  // Here is your change, ma'am.
-  return change;
-}
 
+  // make cid into object array
+  var register = cid.reduce(function(acc, curr) {
+    acc.total += curr[1];
+    acc[curr[0]] = curr[1];
+    return acc;
+  }, {total: 0});
+
+   // is there enough money in the register?
+  if (register.total < change) {
+    return 'Insufficient Funds';
+  }
+  
+  // is change needed?
+  if (register.total === change) {
+    return 'Closed';
+  }
+
+  // Loop through moneys array
+  var change_arr = moneys.reduce(function(acc, curr) {
+    var value = 0;
+    while (register[curr.name] > 0 && change >= curr.val) {
+      change -= curr.val;
+      register[curr.name] -= curr.val;
+      value += curr.val;
+      // Round change to the nearest hundreth
+      change = Math.round(change * 100) / 100;
+    }
+
+    if (value > 0) {
+        acc.push([ curr.name, value ]);
+    }
+    return acc;
+  }, []);
+
+  if (change_arr.length < 1 || change > 0) {
+    return "Insufficient Funds";
+  }
+
+  return change_arr;
+}
 
 checkCashRegister(19.50, 20.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]]);
