d510b2d80ec2ea764992c2993c8ce0d67014cdb6
Amy Carney <digilou@users.noreply.github.com>
2017-03-01 16:27:42 -0900

    Started solution

--- advanced-algorithms/no-repeats-please.js
+++ advanced-algorithms/no-repeats-please.js
@@ -4,12 +4,20 @@ Return the number of total permutations of the provided string that don't have r
 For example, aab should return 2 because it has 6 total permutations (aab, aab, aba, aba, baa, baa), but only 2 of them (aba and aba) don't have the same letter (in this case a) repeating.
 
 - Permutations
-
 - RegExp
 ********/
 
 
 function permAlone(str) {
+  
+  // Heap's algorithm https://en.wikipedia.org/wiki/Heap%27s_algorithm
+  
+  // catch repeating characters
+  const pattern = new RegExp('', 'gi');
+  
+  // permutations should be an array of joined strings
+  
+  // return total number of permutations
   return str;
 }
 
