d9693e3b7982dc55f79b997ff3e1f4bbeb2e4379
Amy Carney <digilou@users.noreply.github.com>
2017-03-05 13:29:03 -0900

    Init make-a-person

---
+++ advanced-algorithms/make-a-person.js
@@ -0,0 +1,27 @@
+/********
+Fill in the object constructor with the following methods below:
+
+    getFirstName()
+    getLastName()
+    getFullName()
+    setFirstName(first)
+    setLastName(last)
+    setFullName(firstAndLast)
+
+Run the tests to see the expected output for each method.
+
+The methods that take an argument must accept only one argument and it has to be a string.
+
+These methods must be the only available means of interacting with the object.
+
+- Closures
+
+- Details of the Object Model
+********/
+
+var Person = function(firstAndLast) {
+    return firstAndLast;
+};
+
+var bob = new Person('Bob Ross');
+bob.getFullName();
