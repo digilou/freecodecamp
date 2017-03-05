e485f8935541d42b06485b9cb6d548f65a0ed274
Amy Carney <digilou@users.noreply.github.com>
2017-03-05 14:00:00 -0900

    Final solution

--- advanced-algorithms/make-a-person.js
+++ advanced-algorithms/make-a-person.js
@@ -20,7 +20,13 @@ These methods must be the only available means of interacting with the object.
 ********/
 
 var Person = function(firstAndLast) {
-    return firstAndLast;
+  var fullName = firstAndLast;
+  this.getFirstName = () => fullName.split(' ')[0];
+  this.getLastName = () => fullName.split(' ')[1];
+  this.getFullName = () => fullName;
+  this.setFirstName = (name) => fullName = name + " " + fullName.split(' ')[1];
+  this.setLastName = (name) => fullName = fullName.split(' ')[0] + " " + name;
+  this.setFullName = (name) => fullName = name;
 };
 
 var bob = new Person('Bob Ross');
